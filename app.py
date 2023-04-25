5dabc894269c3a77d8f69a54246bb15a4741a564
Nattabude <nattabude.t2021@gmail.com>
2023-04-25 17:05:09 +0700

    add auto mode

--- app.py
+++ app.py
@@ -10,47 +10,41 @@ from transformers import pipeline
 
 
 def check_word(sentence) :
+
     input_ids = tokenizer.encode(sentence, return_tensors='pt')
     output = model.generate(input_ids, max_length=512)
     result = tokenizer.decode(output[0], skip_special_tokens=True)
     list_word = [word for word in result.split()]
     dict_candidate = {}
     list_worng_word = []
-
+    
     for idx,i in enumerate (sentence.split()) :
-        if i not in list_word :
-            pred_sent = result.split()
-            pred_sent[idx] = '<mask>'
-            mask_out = mlm(" ".join(pred_sent), top_k=500)
-            for ii in mask_out :
-                if ((ii['token_str']).strip()).startswith(result.split()[idx][:1]) :
-                    list_worng_word.append(ii['token_str'].strip())
-            dict_candidate[i] = list_worng_word[:3]
-            list_worng_word = []
+
+        try:
+
+            if i not in list_word :
+                pred_sent = result.split()
+                pred_sent[idx] = '<mask>'
+                mask_out = mlm(" ".join(pred_sent), top_k=500)
+                for ii in mask_out :
+                    if ((ii['token_str']).strip()).startswith(result.split()[idx][:1]) :
+                        list_worng_word.append(ii['token_str'].strip())
+                dict_candidate[i] = list_worng_word[:3]
+                list_worng_word = []
+        
+        except:
+            pass
+        
     return dict_candidate 
 
-# def check_word(sentence):
-#     input_ids = tokenizer.encode(sentence, return_tensors='pt')
-#     output = model.generate(input_ids, max_length=512)
-#     result = tokenizer.decode(output[0], skip_special_tokens=True)
-#     list_word = [word for word in result.split()]
-#     dict_candidate = {}
+def auto_process(prompt):
 
-#     for idx, i in enumerate(sentence.split()):
-#         if i not in list_word:
-#             pred_sent = result.split()
-#             pred_sent[idx] = '<mask>'
-#             mask_out = mlm(" ".join(pred_sent), top_k=500)
-#             list_wrong_word = []
-#             for ii in mask_out:
-#                 if ii['token_str'].strip().startswith(result.split()[idx][:1]):
-#                     list_wrong_word.append(ii['token_str'].strip())
-#             dict_candidate[idx] = {i: list_wrong_word[:3]}
-
-#     return dict_candidate
-        
-
-    
+    input_ids = tokenizer.encode(prompt, return_tensors='pt')
+    output = model.generate(input_ids, max_length=512)
+    result = tokenizer.decode(output[0], skip_special_tokens=True)
+    if prompt[-1]=="." and (prompt.split()[-1] != result.split[-1]):
+        result = result + "."
+    return result+" "
 
 
 app = Flask(__name__)
@@ -74,6 +68,14 @@ def generate_suggestions():
     # misspell_dict = {"teo":["the","there","this"],"heo":["how","hello","here"],"sdf":["free","span","go"]}
 
     return {'suggestions': misspell_dict}
+
+@app.route('/suggestions_auto')
+def generate_suggestions_auto():
+    prompt = request.args.get('inputText', '')
+    position = request.args.get('cursorPosition', '')
+
+    result = auto_process(prompt)
+    return {'suggestions': result}
 
 
 
--- static/script.js
+++ static/script.js
@@ -7,8 +7,35 @@ document.getElementById('inputText').focus();
 var sel = window.getSelection();
 let countId = 0;
 
+let mode = "manual";
+
 var itxt_Editor = document.getElementById('inputText');
-var debounceTimer;
+var suggestions = document.getElementById('suggestions');
+
+
+
+
+document.getElementById('autoButton').addEventListener('click', function() {
+
+    // Toggle the display property of suggestions list
+    if (itxt_Editor.style.width === '100%') {
+        itxt_Editor.style.width = '70%';
+        suggestions.style.width = '300px'
+    } else {
+        itxt_Editor.style.width = '100%';
+        suggestions.style.width = '0px'
+    }
+
+    if (mode === "manual") {
+        mode = "auto";
+        console.log("auto")
+        
+    } else {
+        mode = "manual";
+        console.log("manual")
+    }
+  });
+
 
 
 function expandOption(optionId) {
@@ -70,6 +97,7 @@ function Ignore(option){
     }
 }
 
+
 function add_misspell(misspell_dict) {
     var suggestionsList = document.getElementById('suggestions');
     suggestionsList.innerHTML = '';
@@ -119,33 +147,149 @@ function add_misspell(misspell_dict) {
     }
 }
 
+function fetch_manual() {
 
+    //   console.log('Space bar pressed!');
+    var input_text = itxt_Editor.innerText;
+    var words = input_text.trim().split(/\s+/);
+    var wordCount = words.length;
+    console.log(input_text);
+    var cursorPosition = sel.anchorOffset;
+    console.log('Text cursor position: ' + cursorPosition);
+    if (wordCount >= 2){
+        // var input_html = itxt_Editor.innerHTML;
+        // const result = createSpans(input_html, countId);
+        // itxt_Editor.innerHTML = result.updatedText;
+        // countId = result.countId;
+        fetch('/suggestions?inputText=' + encodeURIComponent(input_text)+'&cursorPosition=' + cursorPosition)
+            .then(response => response.json())
+            .then(data => {
+                add_misspell(data['suggestions']);
+        
+        });
+    }
+    
+}
+
+function auto_fix(misspell_text,correct_text) {
+
+    const currentText = itxt_Editor.innerText;
+
+    // Replace text using desired logic
+    var replacedText = currentText.replace(misspell_text, correct_text);
+
+    // Set the replaced text back to the div element
+    itxt_Editor.innerText = replacedText;
+
+    const length = itxt_Editor.textContent.length;
+    window.getSelection().collapse(itxt_Editor.firstChild, length);
+}
+
+function fetch_auto(isend) {
+
+    //   console.log('Space bar pressed!');
+    if (isend){
+        var input_text = itxt_Editor.innerText;
+    }else{
+        var full_text = itxt_Editor.innerText;
+        var words = full_text.split(" ");
+        // Extract the text before the last word
+        var input_text = words.slice(0, -1).join(" ");
+    }
+    
+    var words = input_text.trim().split(/\s+/);
+    var wordCount = words.length;
+    console.log(input_text);
+    var cursorPosition = sel.anchorOffset;
+    console.log('Text cursor position: ' + cursorPosition);
+    if (wordCount >= 2){
+
+        fetch('/suggestions_auto?inputText=' + encodeURIComponent(input_text)+'&cursorPosition=' + cursorPosition)
+            .then(response => response.json())
+            .then(data => {
+                auto_fix(input_text,data['suggestions']);
+        
+        });
+    }
+    
+}
+
+
+var debounceTimer;
+let timeoutId;
 
 itxt_Editor.addEventListener('keydown', function(event) {
-    clearTimeout(debounceTimer);
 
-    debounceTimer = setTimeout(function() {
-       
-        //   console.log('Space bar pressed!');
-        var input_text = itxt_Editor.innerText;
-        var words = input_text.trim().split(/\s+/);
-        var wordCount = words.length;
-        console.log(input_text);
-        var cursorPosition = sel.anchorOffset;
-        console.log('Text cursor position: ' + cursorPosition);
-        if (wordCount >= 2){
-            // var input_html = itxt_Editor.innerHTML;
-            // const result = createSpans(input_html, countId);
-            // itxt_Editor.innerHTML = result.updatedText;
-            // countId = result.countId;
-            fetch('/suggestions?inputText=' + encodeURIComponent(input_text)+'&cursorPosition=' + cursorPosition)
-                .then(response => response.json())
-                .then(data => {
-                    add_misspell(data['suggestions']);
-            
-            });
-        }
+    if (mode === "manual"){
+        clearTimeout(debounceTimer);
+
+        debounceTimer = setTimeout(function() {
         
-    },1200);
+            fetch_manual();
+            
+        },1200);
+
+    }else{
+        // check if it enter or space
+        if (event.code === "Space" || event.code === "Enter") {
+            
+            const range = sel.getRangeAt(0);
+            const node = range.startContainer;
+            const text = node.textContent;
+            const textBeforeCursor = text.substring(0, range.startOffset);
+            const lastCharBeforeCursor = textBeforeCursor.slice(-1);
+            
+            // check if it space or enter before 
+            if (!/\s/.test(lastCharBeforeCursor) && lastCharBeforeCursor !== '') {
+                console.log('Space bar or Enter pressed, and text before cursor is not a space or enter.');
+                // Clear the previous timer, if any
+                fetch_auto(false);
+
+                clearTimeout(timeoutId);
+                // Set a new timer to log "press" to the console after 6 seconds of no text input
+                timeoutId = setTimeout(function() {
+                    fetch_auto(true);
+                }, 3000);
+             }
+        }
+
+
+    }
+
 });
 
+// if (mode === "manual"){
+//     var debounceTimer;
+
+//     itxt_Editor.addEventListener('keydown', function(event) {
+//         clearTimeout(debounceTimer);
+
+//         debounceTimer = setTimeout(function() {
+        
+//             fetch_manual();
+            
+//         },1200);
+//     });
+// }else{
+    
+//     let timeoutId;
+//     // Function to be triggered when spacebar is pressed
+//     function handleSpacebarPress() {
+//       console.log("Spacebar pressed!"); // Replace with your desired action
+//       // fetch_auto();
+//     }
+    
+//     // Event listener for keydown event
+//     itxt_Editor.addEventListener('keydown', (event) => {
+//       if (event.code === "Space") { // 32 is the keycode for spacebar
+//         if (timeoutId) {
+//           clearTimeout(timeoutId); // Clear any existing timeout
+//         }
+//         timeoutId = setTimeout(() => {
+//           handleSpacebarPress(); // Trigger the function after 5 seconds of inactivity
+//         }, 5000);
+//       }
+//     });
+      
+
+// }
